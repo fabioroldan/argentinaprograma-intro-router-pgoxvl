456050de568d1341a182beb446c6bedccbc16cb1
fabioroldan <fabioroldan@hotmail.com>
2022-01-24 22:03:35 -0300

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# argentinaprograma-intro-router-pgoxvl
+
+[Edit on StackBlitz ⚡️](https://stackblitz.com/edit/argentinaprograma-intro-router-pgoxvl)
---
+++ src/app/app-routing.module.ts
@@ -0,0 +1,21 @@
+import { NgModule } from '@angular/core';
+import { RouterModule, Routes } from '@angular/router';
+import { AboutComponent } from './about/about.component'
+import { LoginComponent } from './login/login.component'
+import { PerfilComponent } from './perfil/perfil.component'
+import { ServiciosComponent } from './servicios/servicios.component'
+import { FichaJugadorComponent } from './ficha/ficha.component'
+
+const routes: Routes = [
+  {path: 'servicios', component:ServiciosComponent},
+  {path: 'login', component: LoginComponent},
+  {path: 'mi-perfil', component:PerfilComponent},
+  {path: 'sobre-nosotros', component: AboutComponent},
+  {path: 'jugador/:nombre', component: FichaJugadorComponent}
+];
+ 
+@NgModule({
+  imports: [RouterModule.forRoot(routes)],
+  exports: [RouterModule]
+})
+export class AppRoutingModule { }
